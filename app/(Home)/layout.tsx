158328e4f4bfe72a5f6930d3dcb0119145af8dd0
ino <87593133+inoUwU@users.noreply.github.com>
2025-04-25 00:39:02 +0900

    chore: update next.js version to ^15.3.1 in package.json

--- app/(Home)/layout.tsx
+++ app/(Home)/layout.tsx
@@ -1,33 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
 	title: "Fract.Home",
 	description: "Hello,I'm Fract.",
 };
 
-export default function RootLayout({
+export default function HomeLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ja">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+	return <>{children}</>;
 }
